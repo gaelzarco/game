32357aa353d44e512f4518bd95b0988e090f66cc
Gael <gaelxarco@icloud.com>
2026-02-12 15:35:08 -0800

    cmake rm raylib

---
+++ build/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game.dir/link.d"
+  "CMakeFiles/game.dir/game.cxx.o"
+  "CMakeFiles/game.dir/game.cxx.o.d"
+  "game"
+  "game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
